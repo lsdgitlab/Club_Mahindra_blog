d92a1265ddce7fecdaa58e6437cdb4b04d6c94e1
Ls-prafulla <prafulla.patil@lsdigital.com>
2022-10-19 12:50:04 +0530

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// console.log("webpack")
+import $ from "jquery";
+import 'owl.carousel';
+import lozad from 'lozad';
+const observer = lozad();
+observer.observe();
+
+
+// import "./page-animation/barba";
---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+function gotoId(id){
+    let heightOfNav = 0;
+    if($('.acc-header').length){
+        heightOfNav = $('.acc-header').outerHeight(true);
+    }
+    if($("#"+id).length === 1)
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#"+id).offset().top - heightOfNav,
+        }, 1000);
+}
+function playPause(video) { 
+    if (video.paused) 
+        video.play(); 
+    else 
+        video.pause(); 
+} 
+export { gotoId, playPause}
