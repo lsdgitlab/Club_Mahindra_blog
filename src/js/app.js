4ca56fb3c4a12f169b14db9951d23c9821f5aa7c
Shubham <shubham.belge@lsdigital.com>
2022-10-20 16:28:22 +0530

    index-page done

--- src/js/app.js
+++ src/js/app.js
@@ -5,5 +5,19 @@ import lozad from 'lozad';
 const observer = lozad();
 observer.observe();
 
+if ($('#bannerCarousel').length) {
+  let bannerCarousel = $("#bannerCarousel").owlCarousel({
+    margin:0,
+    items: 1,
+    dots:false,
+    nav:true,
+    // center:true,
+    // navText: ["<img src='./../img/arrow.png'>","<img src='./../img/arrow.png'>"],
+    navText: ["<img src='./../img/icon/arrow.svg'>","<img src='./../img/icon/arrow.svg'>"],
+    autoplayTimeout: 5000,
+    // loop:true,
+    smartSpeed: 2000,
+  })
+};
 
 // import "./page-animation/barba";
